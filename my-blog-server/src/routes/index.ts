c521a035634bc7e4d6cffc0e35820fe6aa34fbe0
deisantix <ytasamorim7@gmail.com>
2023-11-15 18:40:29 -0300

    change collection name to constant

--- my-blog-server/src/routes/index.ts
+++ my-blog-server/src/routes/index.ts
@@ -10,10 +10,12 @@ import { getDocumentById, updateOneById } from "../database/utils/query";
 
 const router = Router();
 
+const ARTICLES_COLLECTION = 'articles';
+
 router.put('/api/articles/:articleId/upvote', async (req: Request<{ articleId: string }>, res, next) => {
-    getConnection([ 'articles' ], async (collections: TCollections) => {
+    getConnection([ ARTICLES_COLLECTION ], async (collections: TCollections) => {
         const { articleId } = req.params; 
-        const articlesCollection = collections['articles'];
+        const articlesCollection = collections[ARTICLES_COLLECTION];
         let document: WithId<Document>;
         try {
             document = await getDocumentById(articlesCollection, new ObjectId(articleId));
@@ -31,13 +33,13 @@ router.put('/api/articles/:articleId/upvote', async (req: Request<{ articleId: s
 });
 
 router.post('/api/articles/:articleId/comment', (req: Request<{ articleId: string }, {}, IComment>, res, next) => {
-    getConnection([ 'articles' ], async (collections: TCollections) => {
+    getConnection([ ARTICLES_COLLECTION ], async (collections: TCollections) => {
         const { articleId } = req.params;
         const { postedBy, text } = req.body;
     
         let document: WithId<Document>;
         try {
-            document = await getDocumentById(collections['articles'], new ObjectId(articleId));
+            document = await getDocumentById(collections[ARTICLES_COLLECTION], new ObjectId(articleId));
         } catch (error) {
             return res.sendStatus(404).send('Article not found');
         }
