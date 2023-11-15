a0dd48617193e9c52d2c97fa0fbe1316a133df52
deisantix <ytasamorim7@gmail.com>
2023-11-15 18:46:51 -0300

    force database connection to exist before listening to server

--- my-blog-server/src/database/connection.ts
+++ my-blog-server/src/database/connection.ts
@@ -1,23 +1,24 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, Db } from 'mongodb';
 import { TCollections } from '../types/tcollections';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connect = async () => {
+let db: Db;
+
+export const connect = async (fn: Function) => {
     const connection = process.env.MONGODB_CONNECTION;
     
     let mongodbClient: MongoClient | null = null;
     if (!mongodbClient) {
         mongodbClient = new MongoClient(connection ? connection : '');
         mongodbClient.connect();
-    };
-    
+    }
     const database = process.env.DATABASE;
-    return mongodbClient.db(database ? database : '');
+    db = mongodbClient.db(database ? database : '');
+    fn();
 };
 
 export const getConnection = async (collectionNames: string[], fn: Function) => {
-    const db = await connect();
     const collections: TCollections = {};
     collectionNames.forEach(name => collections[name] = db.collection(name));
     await fn(collections);
--- my-blog-server/src/server.ts
+++ my-blog-server/src/server.ts
@@ -1,10 +1,13 @@
 'use strict';
 import express from 'express';
 import routes from './routes';
+import { connect } from './database/connection';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-
-app.listen(8000, () => console.log('Server is listening on port 8000'));
+connect(() => {
+    console.log("Successfully connected to database");
+    app.listen(8000, () => console.log('Server is listening on port 8000'));
+});
