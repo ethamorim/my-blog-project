c6990d9a7af943bae6bd235bdd218ec8d6708ed5
ethamorim <ytasamorim7@gmail.com>
2023-12-04 15:50:06 -0300

    add logout button

--- my-blog-client/src/components/NavBar.jsx
+++ my-blog-client/src/components/NavBar.jsx
@@ -1,20 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../hooks/useUser";
+import { getAuth, signOut } from "firebase/auth";
 
 const NavBar = () => {
+    const navigate = useNavigate();
+    const { user, isLoading } = useUser();
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-                <li>
-                    <Link to='/articles'>Articles</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/articles'>Articles</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="nav-right">
+                {
+                    user 
+                    ? <button onClick={() => signOut(getAuth())}>Log out</button>
+                    : <button onClick={() => navigate('/login')}>Log in</button>
+                }
+            </div>
+        </>
     );
 };
 
