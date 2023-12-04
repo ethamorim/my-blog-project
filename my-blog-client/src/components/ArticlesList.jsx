22803e5484bf1fbfd7c9203b4318583e26bbcd22
ethamorim <ytasamorim7@gmail.com>
2023-12-04 17:21:29 -0300

    style articles list page

--- my-blog-client/src/components/ArticlesList.jsx
+++ my-blog-client/src/components/ArticlesList.jsx
@@ -4,8 +4,8 @@ const ArticlesList = ({ articles }) => {
     return (
         <>
             {articles.map(article => (
-                <Link key={article.name} to={article.id} className="article-list-item">
-                    <h3>{article.title}</h3>
+                <Link key={article.name} to={article.id} className="articles-list__item">
+                    <h3 className="articles-list__item-title">{article.title}</h3>
                     <p>{article.content[0].substring(0, 150).trim()}...</p>
                 </Link>
             ))}
--- my-blog-client/src/pages/ArticlesListPage.jsx
+++ my-blog-client/src/pages/ArticlesListPage.jsx
@@ -3,7 +3,7 @@ import articles from "../assets/articlesContent";
 
 const ArticlesListPage = () => (
   <>
-    <h1>Articles</h1>
+    <h1 className="articles-list-title">Articles</h1>
     <ArticlesList articles={articles} />
   </>
 );
